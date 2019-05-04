4f4a8c2971f180b04d3235be33a4b6e20d932c1f
Yascha Franz <yascha.franz@tu-dortmund.de>
2019-05-04 23:54:42 +0200

    Aufgabe 1 ohne Schleifen

--- Abgabe-2/scripts/mainscriptw.py
+++ Abgabe-2/scripts/mainscriptw.py
@@ -48,3 +48,66 @@ import scipy.constants as const
 # params = unp.uarray(params, np.sqrt(np.diag(covar)))
 # makeNewTable([convert((r'$c_\text{1}$',r'$c_\text{2}$',r'$T_{\text{A}1}$',r'$T_{\text{A}2}$',r'$\alpha$',r'$D_1$',r'$D_2$',r'$A_1$',r'$A_2$',r'$A_3$',r'$A_4$'),strFormat),convert(np.array([paramsGes2[0],paramsGes1[0],deltat2*10**6,deltat1*10**6,-paramsDaempfung[0]*2,4.48*10**-6 *paramsGes1[0]/2*10**3, 7.26*10**-6 *paramsGes1[0]/2*10**3, (VierteMessung-2*deltat2*10**6)[0]*10**-6 *1410 /2*10**3, unp.uarray((VierteMessung[1]-VierteMessung[0])*10**-6 *1410 /2*10**3, 0), unp.uarray((VierteMessung[2]-VierteMessung[1])*10**-6 *2500 /2*10**3, 0),unp.uarray((VierteMessung[3]-VierteMessung[2])*10**-6 *1410 /2*10**3, 0)]),unpFormat,[[r'\meter\per\second',"",True],[r'\meter\per\second',"",True],[r'\micro\second',"",True],[r'\micro\second',"",True],[r'\per\meter',"",True],[r'\milli\meter',"",True],[r'\milli\meter',"",True],[r'\milli\meter',"",True],[r'\milli\meter',r'1.3f',True],[r'\milli\meter',r'1.3f',True],[r'\milli\meter',r'2.2f',True]]),convert(np.array([2730,2730]),floatFormat,[r'\meter\per\second','1.0f',True])+convert((r'-',r'-'),strFormat)+convert(unp.uarray([57,6.05,9.9],[2.5,0,0]),unpFormat,[[r'\per\meter',"",True],[r'\milli\meter',r'1.2f',True],[r'\milli\meter',r'1.2f',True]])+convert((r'-',r'-',r'-',r'-'),strFormat),convert(np.array([(2730-paramsGes2[0])/2730*100,(2730-paramsGes1[0])/2730*100]),unpFormat,[r'\percent','',True])+convert((r'-',r'-'),strFormat)+convert(np.array([(-paramsDaempfung[0]*2-unp.uarray(57,2.5))/unp.uarray(57,2.5)*100,(4.48*10**-6 *paramsGes1[0]/2*10**3-6.05)/6.05*100, (-7.26*10**-6 *paramsGes1[0]/2*10**3+9.90)/9.90*100]),unpFormat,[r'\percent','',True])+convert((r'-',r'-',r'-',r'-'),strFormat)],r'{Wert}&{gemessen}&{Literaturwert\cite{cAcryl},\cite{alphaAcryl}}&{Abweichung}','Ergebnisse', ['c ','c',r'c','c'])
 
+#Aufgabe 1
+
+N1=6
+N2=N1**2
+
+def W1(x):           #Wahrscheinlichkeit bei gegebener Anzahl an Möglichkeiten für 1 Würfel
+    return x/N1
+
+def W2(x):           #Wahrscheinlichkeit bei gegebener Anzahl an Möglichkeiten für 2 Würfel
+    return x/N2
+
+Einzel = np.arange(N1)+1
+Summe = np.arange(N2).reshape(6,6)
+Summe *=0
+Summe += Einzel
+Summe += Summe.T #Summe von 2 Würfeln
+
+#a)
+
+N9 = np.size(Summe[Summe==9]) #Anzahl an Möglichkeiten für Summe = 9
+a=W9 = W2(N9)                   #Wahrscheinlichkeit für Summe = 9
+
+
+#b)
+
+N9_oder_groesser = np.size(Summe[Summe>=9]) #Anzahl an Möglichkeiten für Summe >= 9
+b=W9_oder_groesser = W2(N9_oder_groesser)     #Wahrscheinlichkeit für Summe >= 9
+
+#c)
+
+N4_5 = 2         #Anzahl an Möglichkeiten für irgendeinen der Beiden Würfel =4 und der andere =5
+c=W4_5 = W2(N4_5)
+
+#d)
+
+N4rot_5blau = 1  #Anzahl der Möglichkeiten für rot=4 und blau=5
+d=W4rot_5blau = W2(N4rot_5blau)
+
+Summe_rot4 = Summe[Einzel==4]  #Elemente der Summe von 2 Würfeln bei denen der erste (rot) =4 ist
+#e)
+
+N9_rot4 = np.size(Summe_rot4[Summe_rot4==9])  #Anzahl an Möglichkeiten bei denen Summe=9 mit rot=4
+e=W9_rot4 = W1(N9_rot4)
+
+#f)
+
+N9_oder_groesser_rot4 = np.size(Summe_rot4[Summe_rot4>=9])  #Anzahl an Möglichkeiten bei denen Summe >= 9 mit rot=4
+f=W9_oder_groesser_rot4 = W1(N9_oder_groesser_rot4)
+
+#g)=d)
+N5blau_rot4 = 1                #Anzahl der Möglichkeiten für rot=4 und blau=5
+g=W5blau_rot4 = W1(N5blau_rot4)
+
+print("A",a)
+print("B",b)
+print("C",c)
+print("D",d)
+print("E",e)
+print("F",f)
+print("G",g)
+
+
+#Aufgabe 2
