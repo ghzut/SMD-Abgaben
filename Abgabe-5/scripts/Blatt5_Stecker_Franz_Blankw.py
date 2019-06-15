b9dca80a82cbb83f0138cb850a0a88ab4020e159
Yascha Franz <yascha.franz@tu-dortmund.de>
2019-06-15 17:02:14 +0200

    Aufgabe 5 Ordner angelegt

---
+++ Abgabe-5/scripts/Blatt5_Stecker_Franz_Blankw.py
@@ -0,0 +1,7 @@
+﻿import numpy as np
+import matplotlib.pyplot as plt
+import scipy.constants as const
+from scipy import integrate
+import pandas as pd
+import numpy.random as rand
+from scipy.stats import stats
