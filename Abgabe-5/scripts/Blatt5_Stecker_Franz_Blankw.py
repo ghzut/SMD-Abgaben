4ea04525fd74173293790207a09c3a80d6c9d253
Yascha Franz <yascha.franz@tu-dortmund.de>
2019-06-16 22:36:10 +0200

    print-Befehle hinzugefügt, fehlender Teil hinzugefügt

--- Abgabe-5/scripts/Blatt5_Stecker_Franz_Blankw.py
+++ Abgabe-5/scripts/Blatt5_Stecker_Franz_Blankw.py
@@ -29,6 +29,10 @@ P_1=df_P_1.to_numpy()
 #a
 mu_0=P_0.mean(axis=0)
 mu_1=P_1.mean(axis=0)
+print("mu_0=")
+print(mu_0)
+print("mu_1=")
+print(mu_1)
 #mu_01=P_01.mean(axis=0)
 #b
 def Covariance_2D(x,mu):
@@ -45,10 +49,17 @@ V_1=Covariance_2D(P_1,mu_1)
 #V_01=Covariance_2D(P_01,mu_01)
 V_01=V_0+V_1
 
+print("V_0")
+print(V_0)
+print("V_1")
+print(V_1)
+print("V_01")
+print(V_01)
 #c
 lambda_=-alg.inv(V_01) @ (mu_0 - mu_1)
 e_lambda=lambda_/alg.norm(lambda_)  #wird zur x-Achse mittels Drehung
-
+print("e_lambda")
+print(e_lambda)
 #d
 #z_proj=np.array([1,-lambda_[0]/lambda_[1]])
 #e_z=z_proj/alg.norm(z_proj)         #wird zur y-Achse mittels Drehung
@@ -93,7 +104,7 @@ plt.legend(loc='best')
 plt.savefig('build/Aufgabe_2_e.pdf')
 plt.clf()
 
-#f             durch 0 teilen und ähnliches macht diesen Aufgabenteil nicht möglich meiner meinung nach...
+#f             durch 0 teilen und ähnliches macht diesen Aufgabenteil nicht ohne modifikation möglich meiner meinung nach...
 #def SB(l,Signal,Underground,left=True):          #size of Signal/Underground at specific l  Original (Sieht bescheuert aus)
 #    if left==False:l=-l
 #    x=np.size(Underground[Underground<l[0]])
@@ -116,7 +127,7 @@ def SB(l,Signal,Underground,left=True):          #size of Signal/Underground at 
 y=SB(lambda_cut,P_0_proj,P_1_proj)
 plt.plot(lambda_cut,y)
 lambda_SB_max=lambda_cut[np.argmax(y)]
-print("Für P_0_10000: \lambda_{S/B_{max}}")
+print("\lambda_{S/B_{max}}")
 print(lambda_SB_max)
 #plt.vlines(lambda_SB_max,0,np.max(y))
 plt.xlabel(r'$\lambda$')
@@ -126,20 +137,48 @@ plt.savefig('build/Aufgabe_2_f.pdf')
 plt.clf()
 
 #g
+def significance(l,Signal,Underground,left=True):           #abgewandelte Formel wegen durch null teilen
+    if left==False:l=-l
+    S_B=np.zeros(1,dtype=float)
+    S_B[0]=(np.size(Signal[Signal<l[0]])+1)/(np.size(Signal[Signal<l[0]])+np.size(Underground[Underground<l[0]])+1)**(1/2)
+    for i in range(1,np.size(l)):
+        S_B=np.append(S_B,(1+np.size(Signal[Signal<l[i]]))/(np.size(Signal[Signal<l[i]])+np.size(Underground[Underground<l[i]])+1)**(1/2))
+    return S_B
+
+y=significance(lambda_cut,P_0_proj,P_1_proj)
+plt.plot(lambda_cut,y)
+lambda_S_max=lambda_cut[np.argmax(y)]
+print("\lambda_{Signifikanz_{max}}")
+print(lambda_S_max)
+#plt.vlines(lambda_SB_max,0,np.max(y))
+plt.xlabel(r'$\lambda$')
+plt.ylabel(r'Signifikanz')
+#plt.legend(loc='best')
+plt.savefig('build/Aufgabe_2_g.pdf')
+plt.clf()
+
+#h
 #Strg+c;Strg+v und löschen redundanten Codes
 df_P_0 = pd.read_hdf("zwei_populationen.h5", "P_0_1000")
 
 P_0=df_P_0.to_numpy()
 mu_0=P_0.mean(axis=0)
+print("mu_0=")
+print(mu_0)
 #b
 
 V_0=Covariance_2D(P_0,mu_0)
 V_01=V_0+V_1
+print("V_0")
+print(V_0)
+print("V_01")
+print(V_01)
 
 #c
 lambda_=-alg.inv(V_01) @ (mu_0 - mu_1)
 e_lambda=lambda_/alg.norm(lambda_)  #wird zur x-Achse mittels Drehung
-
+print("e_lambda")
+print(e_lambda)
 #d
 P_0_proj=e_lambda@(P_0.T)
 P_1_proj=e_lambda@(P_1.T)
@@ -147,7 +186,7 @@ plt.hist(P_0_proj,bins=20,alpha=0.5,label=r'$P_0$')
 plt.hist(P_1_proj,bins=20,alpha=0.5,label=r'$P_1$')
 plt.legend(loc='best')
 plt.xlabel(r'$\lambda$')
-plt.savefig('build/Aufgabe_2_gd.pdf')
+plt.savefig('build/Aufgabe_2_hd.pdf')
 plt.clf()
 
 #e
@@ -156,18 +195,31 @@ plt.plot(lambda_cut,recall(lambda_cut,P_0_proj),label='Effizienz')
 plt.plot(lambda_cut,precision(lambda_cut,P_0_proj,P_1_proj),label='Reinheit')
 plt.xlabel(r'$\lambda$')
 plt.legend(loc='best')
-plt.savefig('build/Aufgabe_2_ge.pdf')
+plt.savefig('build/Aufgabe_2_he.pdf')
 plt.clf()
 
-#f             durch 0 teilen und ähnliches macht diesen Aufgabenteil nicht möglich meiner meinung nach...
+#f             durch 0 teilen und ähnliches macht diesen Aufgabenteil nicht ohne modifikation möglich meiner meinung nach...
 y=SB(lambda_cut,P_0_proj,P_1_proj)
 plt.plot(lambda_cut,y)
 lambda_SB_max=lambda_cut[np.argmax(y)]
-print("Für P_0_1000: \lambda_{S/B_{max}}")
+print("\lambda_{S/B_{max}}")
 print(lambda_SB_max)
 #plt.vlines(lambda_SB_max,0,np.max(y))
 plt.xlabel(r'$\lambda$')
 plt.ylabel(r'$\frac{Signal}{Untergrund}$')
 #plt.legend(loc='best')
-plt.savefig('build/Aufgabe_2_gf.pdf')
+plt.savefig('build/Aufgabe_2_hf.pdf')
+plt.clf()
+
+#g
+y=significance(lambda_cut,P_0_proj,P_1_proj)
+plt.plot(lambda_cut,y)
+lambda_S_max=lambda_cut[np.argmax(y)]
+print("\lambda_{Signifikanz_{max}}")
+print(lambda_S_max)
+#plt.vlines(lambda_SB_max,0,np.max(y))
+plt.xlabel(r'$\lambda$')
+plt.ylabel(r'Signifikanz')
+#plt.legend(loc='best')
+plt.savefig('build/Aufgabe_2_hg.pdf')
 plt.clf()
