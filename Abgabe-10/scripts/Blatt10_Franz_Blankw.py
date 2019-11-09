5cdda48e2b89646fa01ea605edd3fc59e26f6a0e
Yascha Franz <yascha.franz@tu-dortmund.de>
2019-11-09 17:02:48 +0100

    Abgabe 10 erstellt

---
+++ Abgabe-10/scripts/Blatt10_Franz_Blankw.py
@@ -0,0 +1,21 @@
+﻿import numpy as np
+import matplotlib.pyplot as plt
+import scipy.constants as const
+from scipy.optimize import curve_fit
+#from scipy import integrate
+#import pandas as pd
+import numpy.random as rand
+#from scipy.stats import stats
+#import numpy.linalg as alg
+#from sklearn import datasets
+#from sklearn.datasets import make_blobs as m_b
+#from sklearn.decomposition import PCA
+
+
+#Aufgabe 25
+
+#Aufgabe 26
+
+#Aufgabe 27
+
+#Aufgabe 28
