d878907588400f5e664621d1b9779bdddd1fa3b6
Yascha Franz <yascha.franz@tu-dortmund.de>
2019-12-16 19:15:41 +0100

    Aufgabe 35 fertig

--- Abgabe-12/scripts/Blatt12_Franz_Blankw.py
+++ Abgabe-12/scripts/Blatt12_Franz_Blankw.py
@@ -14,4 +14,41 @@ import numpy.random as rand
 
 #Aufgabe 35
 
+def f(g,epsilon):
+    return np.array([[epsilon-1,epsilon],[epsilon,epsilon-1]]) @ g/(2*epsilon-1)
+
+def V(sigma_g,epsilon):
+    cov=epsilon*(epsilon-1)*np.sum(sigma_g**2)
+    sigma_1 = (epsilon-1)**2 * sigma_g[0]**2 + epsilon**2 * sigma_g[1]**2
+    sigma_2 = (epsilon-1)**2 * sigma_g[1]**2 + epsilon**2 * sigma_g[0]**2
+    V_ = np.array([[sigma_1,cov],[cov,sigma_2]])
+    return V_/(2*epsilon-1)**2
+
+def sigma(V):
+    return np.sqrt(np.diag(V))
+
+def rho(V):
+    sigma_ = sigma(V)
+    return V[0,1]/np.prod(sigma_)
+
+#d)
+
+def analysis(g1, g2, epsilon):
+    g = np.array([g1,g2])
+    print("f(",g,",",epsilon,"):")
+    print(f(g, epsilon))
+    V_ = V(g, epsilon)
+    print("V:")
+    print(V_)
+    print("\sigma_f:")
+    print(sigma(V_))
+    print("\\rho:")
+    print(rho(V_))
+
+analysis(200.0,169.0,0.1)
+#e)
+analysis(200.0,169.0,0.4)
+#f)
+analysis(200.0,169.0,0.5)
+
 #Aufgabe 36
